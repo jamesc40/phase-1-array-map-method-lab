5a1e633a2025461115bb0cb9359d53245446d2d2
James Coffman <jamesc4023@gmail.com>
2022-04-06 17:03:40 -0400

    commit tree 8e8f8eecbfaeddd7a9b5fbf69c64feb751257dbd

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
+let newArray = []
 
-const titleCased = () => {
-  return tutorials
+const getTitleCase = tutorials.map(function (sentence){
+  let titleCase = sentence.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  return newArray.push(titleCase)
+})
+function titleCased(){
+  return newArray
 }
